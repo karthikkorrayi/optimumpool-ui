0e830d7b4d1db99f7402dba3fa12f5ffcf231f5a
Karthik Korrayi <karthikkorrayii@gmail.com>
2026-06-19 15:17:23 +0530

    Build and Deploy Frontend to Azure App Service

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  authApi:    'http://optimumpool-app.southindia.azurecontainer.io:8020',
-  offerApi:   'http://optimumpool-app.southindia.azurecontainer.io:8086',
-  bookingApi: 'http://optimumpool-app.southindia.azurecontainer.io:8084'
+  authApi:    '/api/auth',
+  offerApi:   '/api/offer',
+  bookingApi: '/api/booking'
 };
